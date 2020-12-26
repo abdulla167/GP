e8aa3a8f41a1e91bc83281ceb126d8da631cbfe3
abdulla167 <abdullaelsayed167@yahoo.com>
2020-12-27 01:11:03 +0200

    first commit

---
+++ web application/src/app/user.service.ts
@@ -0,0 +1,11 @@
+import {User} from './models/user.model';
+
+export class UserService{
+  registerUser(user : User){
+    // send user to be saved in the database
+  }
+
+  authenicateUser(user :User){
+    // send user to back-end to athenicate and get his data
+  }
+}
